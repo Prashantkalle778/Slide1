006787ae238f98276b9d6549ad1a13fe65ac2224
Prashantkalle778 <156544748+Prashantkalle778@users.noreply.github.com>
2025-09-16 12:26:11 +0530

    slide1

---
+++ story_content/triggers.js
@@ -0,0 +1,89 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5mC8eksSUQD":
+        Script1();
+        break;
+      case "5cQx7zFlZXF":
+        Script2();
+        break;
+      case "6E8dDqrOlOl":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5niGDCJBudl');
+const duration = 2000;
+const easing = 'ease-out';
+const id = '5cAcielWmMl';
+const pulseAmount = 0.07;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  const target = object('5niGDCJBudl');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6HEAjUKbJVU';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('5niGDCJBudl');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6HEAjUKbJVU_reverse';
+const growAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
